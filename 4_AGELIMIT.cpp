23cd0f42165c929096842cf5c3afd11b24d60223
Ajinkya Shinde <ajinkyashinde818@gmail.com>
2026-09-05 19:37:26 +0530

    Basic Programming Assignment 1

---
+++ 4_AGELIMIT.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+ int t;
+ cin>>t;
+ while(t--)
+  { 
+   int X,Y,A;
+   //Accept 3 integers inputs.
+   cin>>X>>Y>>A;    
+    if (A>=X && A<Y){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+
+   }
+ return 0;
+}
